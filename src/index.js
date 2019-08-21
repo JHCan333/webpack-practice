0d4c9277052bdfaca2b38c9de262493fd927a5f1
jinhongcan <jinhongcan@baiwang.com>
2019-08-21 10:31:52 +0800

    完成管理输出模块的练习

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
 import _ from 'lodash'
-import './style.css'
-import bwImg from './bwImg.png'
+import './static/css/index.css'
+import printMe from './print'
 
 function component () {
     var element = document.createElement('div')
+    var btn = document.createElement('button')
 
     // Lodash, currently included via a script, is required for this line to work
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ')
     element.classList.add('hello')
 
-    // 将图像添加到现有的div
-    var bwImg = new Image();
-    bwImg.src = bwImg
-    element.appendChild(bwImg)
+    btn.innerHTML = 'Click me and check the console!'
+    btn.onclick = printMe
+    element.appendChild(btn)
 
     return element
 }
--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,12 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        app: './src/index.js',
+        print: './src/print.js'
+    },
     output: {
-        filename: '[hash]bundle.js',
+        filename: '[name]-[hash].js',
         path: path.resolve(__dirname, 'dist')
     },
     plugins: [
